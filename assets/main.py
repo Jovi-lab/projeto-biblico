333c78a6156a176a425033b9e50b33176f0ebd67
Jovi <joaovitorvicente2002@gmail.com>
2026-05-18 02:25:36 -0300

    add MotorNarrativa with choices and progress system

--- assets/main.py
+++
@@ -1,7 +0,0 @@
-from src.usuario import Usuario
-
-jovi = Usuario("Jovi")
-jovi.iniciar_historia("Davi e Golias")
-jovi.ganhar_pontos(50)
-jovi.atualizar_progresso("Davi e Golias", 60)
-jovi.status()
